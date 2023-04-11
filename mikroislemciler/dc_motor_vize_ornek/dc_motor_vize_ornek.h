9fb97d5464f16a621076967f8b4f0ca7f5ed31e3
Gokay Egilmez <68199226+starwarsawekand22@users.noreply.github.com>
2023-04-12 00:13:37 +0300

    Add files via upload

---
+++ mikroislemciler/dc_motor_vize_ornek/dc_motor_vize_ornek.h
@@ -0,0 +1,7 @@
+#include <10F220.h>
+#device ADC=16
+
+#FUSES NOWDT                 	//No Watch Dog Timer
+
+#use delay(internal=8000000)
+
---
+++ mikroislemciler/trafik_isiklari_ornek/trafik_isiklari.c
@@ -0,0 +1,63 @@
+#include <16f877a.h>
+#fuses xt, nowdt
+#use delay(clock=4mhz)
+unsigned int16 i;// zamaný bununla tanýmlýcaz 16 bitlik tanýmladýk saklayýcýyý.
+//***************************************
+void test();
+{
+   if(input(pin_a1)==0 || input(pin_a2)==0)
+   {
+      output_b(0b00001100);
+      output_c(0b00001100);
+      delay_ms(1000);
+      output_b(0b10000011);
+      output_c(0b10000011);
+   }
+}
+
+
+
+void main()
+{
+   output_b(0); output_c(0);
+   while(TRUE)
+   {
+      output_b(0b10000011);
+      output_c(0b10000011); // binary formatta yazdýk bilerek 0 lar sönük 1 ler yanýk b partýna baðlý olan ledler
+      
+      
+      for(i=0; i<500; i++) //zamaný parçaladýk burda
+      {
+         test();
+         delay_ms(10);
+         if(i == 400)
+         {
+            output_b(0b10001111); //sarýlarý yaktýk 4. sn ye de
+            output_c(0b10001111);
+         }
+      }
+      // buraya kadar ilk 5 sn yi yazdýk yayalara yeþil arabalara kýrmýzý ile baþladýk 
+      
+      
+      output_b(0b01110000); 
+      output_c(0b01110000);
+      for(i=0; i<500; i++) //zamaný parçaladýk burda
+      {
+         test();
+         delay_ms(10);
+         if(i == 400)
+         {
+            output_b(0b01111100); //sarýlarý yaktýk 4. sn ye de
+            output_c(0b01111100);
+         }
+      }
+      
+      // burada da  bir periyot bitti.
+      
+      
+      
+      
+      
+   }
+
+}
